bbb54ea5ceb2df7f9fe20f003c0004a41f45f28b
Fran Lorda <franciscolorda@heytutor.com>
2024-08-30 21:31:10 -0300

    change server port

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const controller = require("./controllers/snapController");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 const logger = winston.createLogger({
   level: "info",
